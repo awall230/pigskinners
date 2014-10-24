a5865c864606b71f38834042bf304714966180f6
Adam Waller <awaller@u.rochester.edu>
2014-10-24 13:53:06 -0400

    Added new database view

---
+++ public_html/create_allusersbets_view.sql
@@ -0,0 +1,1 @@
+create view allusersbets as select email, g.game_id, g.team_a, g.team_b, g.date, g.time, g.status, b.bet_type, b.margin, b.american_odds, b.odds from games g, bets b where g.game_id = b.game_id;
