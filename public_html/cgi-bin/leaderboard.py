d80293572981df05743d9afdeb955f577018e5f1
Adam Waller <awaller@u.rochester.edu>
2014-11-25 16:28:09 -0500

    fixed python version bug

--- public_html/cgi-bin/leaderboard.py
+++ public_html/cgi-bin/leaderboard.py
@@ -39,8 +39,8 @@ for user in users:
 
 	if email: #want info for a specific user's profile
 		if user[0] == email:	#this is the user's data
-			data['total_winnings'] = '{:.2f}'.format(total_winnings)
-			data['average_winnings'] = '{:.2f}'.format(average_winnings)
+			data['total_winnings'] = '{0:.2f}'.format(total_winnings)
+			data['average_winnings'] = '{0:.2f}'.format(average_winnings)
 
 ranks.sort(reverse=True);	#sort by average winnings
 
@@ -54,8 +54,8 @@ if email:	#want info for a specific user's profile
 else:	#this is for displaying global leaderboard
 	data['leaders'] = []
 	for i in range(len(ranks)):
-		data['leaders'].append( {'rank': i, 'email': ranks[i][3], 'average_winnings': '{:.2f}'.format(ranks[i][0]),
-			'total_winnings': '{:.2f}'.format(ranks[i][1]), 'bet_count': ranks[i][2]} )
+		data['leaders'].append( {'rank': i, 'email': ranks[i][3], 'average_winnings': '{0:.2f}'.format(ranks[i][0]),
+			'total_winnings': '{0:.2f}'.format(ranks[i][1]), 'bet_count': ranks[i][2]} )
 
 #print JSON
 print "Content-type: application/json"
